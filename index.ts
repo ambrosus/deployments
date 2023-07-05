6ce7a239236752c5ac9b366d609f39db586f43fe
serezhaolshan <olshan228@gmail.com>
2023-07-05 17:22:52 +0300

    feat: add index file

---
+++ index.ts
@@ -0,0 +1,11 @@
+import { deploy } from './deploy';
+import { loadAllDeployments, loadDeployment } from './deployments';
+import { sourcifyAll, sourcifyOne } from './sourcify';
+
+export {
+  deploy,
+  loadDeployment,
+  loadAllDeployments,
+  sourcifyAll,
+  sourcifyOne
+}
