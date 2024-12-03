d70b33aaa1498961097cc220ba887c02285d0993
SigismundSchlomo <o.kuznietsov@inc4.net>
2024-12-03 20:43:06 +0200

    fix: remove migration and update imports

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,1 @@
 export {loadAllDeployments, loadAllDeploymentsFromFile, loadDeployment} from "./deployments"
-export {
-  getMultisigFactory,
-  type MultisigFactoryContract,
-  type MultisigSettings
-} from "./deploying/deploy"
-export {
-  migrateDeployments,
-  type MigrationOptions,
-  type OldDeployment,
-  type NewDeployment
-} from "./deploying/migrate"
